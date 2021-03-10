9a2fc8c2bc7021b82532956b76825debcfeef763
hyojin <gaya309@naver.com>
2021-03-10 21:10:33 +0900

    create SingletonWithPrototypeTest1.java and add dependency on the web

---
+++ spring/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,89 @@
+package hello.core.scope;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Provider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+    @Scope("singleton")
+    static class ClientBean{
+   //     private final PrototypeBean prototypeBean; //생성 시점에 주입
+
+//        //ObjectProvider 사용 예제
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;//스프링에 의존적
+//
+//
+//        public int logic(){
+//            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();//항상 새로운 프로토타입 빈이 생성된다.(스프링 컨테이너를 통해 해당 빈을 찾아서 반환)
+//            prototypeBean.addCount();
+//            int count = prototypeBean.getCount();
+//            return count;
+//        }
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanObjectProvider;//자바 표준
+
+
+        public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();//항상 새로운 프로토타입 빈이 생성된다.
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+    @Scope("prototype")
+    static class PrototypeBean{
+        private int count = 0;
+
+        public void addCount(){
+            count++;
+        }
+
+        public int getCount(){
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public  void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
